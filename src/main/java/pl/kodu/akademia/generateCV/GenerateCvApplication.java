41ad2f08d64782b999f22bd81dba6226f9764d27
Zacharsky <mateusz.zacharski1@gmail.com>
2018-09-30 12:36:41 +0200

    first commit

---
+++ src/main/java/pl/kodu/akademia/generateCV/GenerateCvApplication.java
@@ -0,0 +1,12 @@
+package pl.kodu.akademia.generateCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateCvApplication.class, args);
+	}
+}
